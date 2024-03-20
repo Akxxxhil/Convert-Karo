416ad71e08fa5da1f6e6d627d2b69743a56f5e7d
Akxxxhil <1941012872.e.anshumanbiswal@gmail.com>
2024-03-20 18:11:11 +0530

    updated

--- src/Components/Text.jsx
+++ src/Components/Text.jsx
@@ -4,26 +4,32 @@ import { toast } from "react-toastify";
 
 function Text() {
     const [text, setText] = useState("");
-    
+
     function changehandler(event) {
-        
-        setText(event.target.value)
+        setText(event.target.value);
     }
-    function uppercasehandler(){
-        let newtext=text.toUpperCase()
-        setText(newtext)
-        toast.success("Converted to Uppercase")
-        
+    function uppercasehandler() {
+        if (text == "") {
+            toast.warn("Please Write Something");
+        } else {
+            let newtext = text.toUpperCase();
+            setText(newtext);
+            toast.success("Converted to Uppercase");
+        }
     }
-    function lowercasehandler(){
-        let newtext=text.toLowerCase()
-        setText(newtext)
-        toast.success("Converted to Lowercase")
+    function lowercasehandler() {
+        if (text == "") {
+            toast.warn("Please Write Something");
+        } else {
+            let newtext = text.toLowerCase();
+            setText(newtext);
+            toast.success("Converted to Lowercase");
+        }
     }
-    function Cleartexthandler(){
-        let newtext=""
-        setText(newtext)
-        toast.success("Text Cleared")
+    function Cleartexthandler() {
+        let newtext = "";
+        setText(newtext);
+        toast.success("Text Cleared");
     }
     return (
         <>
@@ -31,26 +37,30 @@ function Text() {
                 <h1> Text to Likho Pahle</h1>
             </div>
             <div className="textareadiv">
-               <textarea value={text} onChange={changehandler}    name="" id="" cols="150" rows="10"></textarea>
-             </div>
-             <div className="buttons">
+                <textarea
+                    value={text}
+                    onChange={changehandler}
+                    name=""
+                    id=""
+                    cols="150"
+                    rows="10"
+                ></textarea>
+            </div>
+            <div className="buttons">
                 <div onClick={uppercasehandler}>Convert to Uppercase</div>
                 <div onClick={lowercasehandler}>Convert to Lowercase</div>
                 <div onClick={Cleartexthandler}>Clear Text</div>
-             </div>
-             <div className="container">
+            </div>
+            <div className="container">
                 <h2>Your Text Summary</h2>
-                <p>{text.split(" ").length} words and {text.length} characters</p>
-                <p>{0.008*text.split(" ").length} minutes taken to Read</p>
+                <p>
+                    {text.split(" ").length} words and {text.length} characters
+                </p>
+                <p>{0.008 * text.split(" ").length} minutes taken to Read</p>
                 <h2>Your Preview</h2>
                 <p>{text}</p>
-             </div>
+            </div>
         </>
     );
 }
 export default Text;
-
-
- 
- 
-
